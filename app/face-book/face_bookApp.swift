b22dea0ac824d846999becfa8792fbd90bdc2903
Fraser <tomatoesneverlie@gmail.com>
2024-01-27 13:36:59 -0500

    create xcode project

---
+++ app/face-book/face_bookApp.swift
@@ -0,0 +1,32 @@
+//
+//  face_bookApp.swift
+//  face-book
+//
+//  Created by Fraser Lee on 2024-01-27.
+//
+
+import SwiftUI
+import SwiftData
+
+@main
+struct face_bookApp: App {
+    var sharedModelContainer: ModelContainer = {
+        let schema = Schema([
+            Item.self,
+        ])
+        let modelConfiguration = ModelConfiguration(schema: schema, isStoredInMemoryOnly: false)
+
+        do {
+            return try ModelContainer(for: schema, configurations: [modelConfiguration])
+        } catch {
+            fatalError("Could not create ModelContainer: \(error)")
+        }
+    }()
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+        .modelContainer(sharedModelContainer)
+    }
+}
