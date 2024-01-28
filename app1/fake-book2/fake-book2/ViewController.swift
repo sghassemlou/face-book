b7385fdeb779b0d557f21d2d3cdcf0d92a09d926
Fraser <tomatoesneverlie@gmail.com>
2024-01-28 05:55:28 -0500

    fb2

---
+++ app1/fake-book2/fake-book2/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  fake-book2
+//
+//  Created by Fraser Lee on 2024-01-28.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
