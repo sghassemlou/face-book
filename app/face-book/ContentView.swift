c206ca962eacf8c756cb964a148e4d12b421f14b
Fraser <tomatoesneverlie@gmail.com>
2024-01-27 20:17:26 -0500

    correct view

--- app/face-book/ContentView.swift
+++ app/face-book/ContentView.swift
@@ -11,31 +11,40 @@ import SwiftData
 struct ContentView: View {
     @Environment(\.modelContext) private var modelContext
     
-    var viewControl = HostedViewController()
-
     var body: some View {
         // rounded rect taking up the top half of the screen with padding
         GeometryReader{ geo in
             VStack{
-                viewControl
+                HostedViewController()
                     .frame(height: geo.size.height * (1/2))
                     .cornerRadius(25.0)
                     .padding()
 
             
-                VStack{
-                    Button (action: toggleCam) {
-                        Image(systemName: "camera.rotate.fill")
-                            .frame(width: 52, height: 52)
-                            .foregroundColor(.white)
-                            .background(.gray)
-                            .cornerRadius(15)
-                            .imageScale(.large)
+                GeometryReader{ geo1 in
+                    VStack (alignment: .leading) {
+                        HStack (alignment: .top){
+                            Button (action: toggleCam) {
+                                Image(systemName: "camera.rotate.fill")
+                                    .frame(width: 52, height: 52)
+                                    .foregroundColor(.white)
+                                    .background(.gray)
+                                    .cornerRadius(15)
+                                    .imageScale(.large)
+                                
+                            }
+                        }
+                        
+                        Text("hello world.")
+                            .font(.system(size: 25, weight: .regular, design: .rounded))
+                            .frame(width: geo.size.width)
                             
-                    }
-                    Text("hello world.")
-                        .font(.system(size: 25, weight: .regular, design: .rounded))
-                }.frame(height: geo.size.height * (1/2))
+                    }.frame(width: geo.size.width)
+                }
+                .background(Color.red)
+                .frame(height: geo.size.height * (1/2))
+                .padding()
+
             }
         }
     }
